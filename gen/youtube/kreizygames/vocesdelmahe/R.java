07726ec856eccc19e9c3991393345db9ad1d8791
Eljodiopapa <gomez.malmierca.rodrigo@gmail.com>
2013-02-05 16:25:36 +0100

    Quitando pruebas varias

--- gen/youtube/kreizygames/vocesdelmahe/R.java
+++ gen/youtube/kreizygames/vocesdelmahe/R.java
@@ -15,8 +15,7 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int menu_settings=0x7f070000;
     }
     public static final class layout {
         public static final int activity_voces_del_mahe=0x7f030000;
@@ -26,7 +25,6 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int boton=0x7f040004;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
         public static final int title_activity_voces_del_mahe=0x7f040003;
--- src/youtube/kreizygames/vocesdelmahe/VocesDelMahe.java
+++ src/youtube/kreizygames/vocesdelmahe/VocesDelMahe.java
@@ -19,16 +19,4 @@ public class VocesDelMahe extends Activity {
         getMenuInflater().inflate(R.menu.activity_voces_del_mahe, menu);
         return true;
     }
-    
-    public double sumar(int num1, double num2) {
-    	double total = num1+num2;
-    	return total;
-    	
-    }
-    
-    public void bitches(View v) {
-    	double resultadoSuma = sumar(8, 8.8);
-    	Toast.makeText(getApplicationContext(), "Total de suma " + resultadoSuma, Toast.LENGTH_LONG).show();
-    	// ola k ase
-    }
 }
