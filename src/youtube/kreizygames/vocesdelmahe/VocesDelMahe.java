e8f138ca6d09c6b1e50befa28e447acfbd82874e
Eljodiopapa <gomez.malmierca.rodrigo@gmail.com>
2013-02-07 00:51:31 +0100

    Ahi tienes tu ejemplo de sonido xD

--- src/youtube/kreizygames/vocesdelmahe/VocesDelMahe.java
+++ src/youtube/kreizygames/vocesdelmahe/VocesDelMahe.java
@@ -1,22 +1,28 @@
 package youtube.kreizygames.vocesdelmahe;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Toast;
 
 public class VocesDelMahe extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_voces_del_mahe);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_voces_del_mahe);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_voces_del_mahe, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_voces_del_mahe, menu);
+		return true;
+	}
+
+	public void voz1(View v) {
+		MediaPlayer reproSonido = MediaPlayer.create(VocesDelMahe.this,
+				R.raw.gato);
+		reproSonido.start();
+	}
 }
